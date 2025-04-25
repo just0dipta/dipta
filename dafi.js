c21980326d93ff4908f883b2d9c33f20e66bcf2d
just0dipta <diptamahijav@gmail.com>
2025-04-25 08:25:47 +0700

    Update dafi.js

--- dafi.js
+++ dafi.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["coder", "webdev", "gamedev"],
+        strings: ["designer", "webdeg", "gamedeg"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["coder", "webdev", "gamedev"],
+        strings: ["designer", "webdeg", "gamedeg"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
